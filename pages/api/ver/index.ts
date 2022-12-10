d4672e192238ab3519cdb5e31377f9f1224f99d0
sean <sean@imaginato.com>
2022-12-11 01:46:48 +0800

    1

--- pages/api/ver/index.ts
+++ pages/api/ver/index.ts
@@ -4,5 +4,5 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
 
       // Get data from your database
-      res.status(200).send("v1.0.2")  
+      res.status(200).send("v1.0.3")  
 }
