7490221479d9c506b96596cb01aa0a1641cd0d80
sean <sean@imaginato.com>
2022-12-11 01:45:15 +0800

    1

--- pages/api/event/events_handlers/_message.ts
+++ pages/api/event/events_handlers/_message.ts
@@ -5,7 +5,7 @@ export async function message(req, res) {
     let event = req.body.event;
     let channel = event.channel;
     let thread = event.ts;
-    let text = `In message! <@${event.item.user}>!`; 
+    let text = `In message! <@${event.user}>!`; 
     
     try {
         await postToChannel(channel, thread, res, "In Message. Sending to event.channel "+channel + " event.ts "+thread);
