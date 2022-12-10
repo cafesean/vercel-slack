65e363c439eba7a057042b32129b25ceb83c9da8
sean <sean@imaginato.com>
2022-12-11 00:11:28 +0800

    testing

--- pages/api/event/events_handlers/_app_mention.ts
+++ pages/api/event/events_handlers/_app_mention.ts
@@ -8,9 +8,20 @@ export async function app_mention(req:any, res:any) {
     let text = `Hi there! Thanks for mentioning me, <@${event.item.user}>!`; 
     
     try {
-        await postToChannel(channel, thread, res, text);
+        await postToChannel(channel, thread, res, "Sending to event.channel "+channel + " event.ts "+thread);
     }
     catch (e) {
         console.log(e);
     }
+
+    try {
+        await postToChannel(event.item.channel, event.item.ts, res, "Sending to event.item.channel "+event.item.channel + " event.item.ts "+event.item.ts);
+    }
+    catch (e) {
+        console.log(e);
+    }
+
+
+
+
 }
